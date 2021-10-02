c6e3be2cf07907e03c4371cdc5dafd894a2707dc
Kotarororororo <someya.kotaro.infratop@gmail.com>
2021-10-02 05:55:05 +0000

    [Start] bookers2作成

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root to: 'homes#top'
+  devise_for :users
+  resources :books, only: [:create, :index, :show, :destroy]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
