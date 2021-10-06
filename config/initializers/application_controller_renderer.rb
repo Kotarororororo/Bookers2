6a0f3976e80e4f7f7e586b6e27b640ffb2585dc5
Kotarororororo <someya.kotaro.infratop@gmail.com>
2021-10-06 00:18:18 +0000

    [Finish] bookers2作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '418f29eb65c5ee71ac22809ca7aa1f1d97f79b16699c3252e5190bd784fee4db2b22109870c94210a6576cc6b65a5685a447d4b5ab5c4f3baa768f1d43ddf87b'
